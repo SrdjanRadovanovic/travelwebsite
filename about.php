2f2be2c2a062f5cb7d7aa1e724e8a5016aedec46
Srdjan <82110018+SrdjanRadovanovic@users.noreply.github.com>
2022-09-07 16:17:26 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+ <!-- header section starts  -->
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+ <!-- header section ends -->
+
+<div class="heading" style="background:url('images/header-bg-1.png') no-repeat;">
+   <h1>about us</h1>
+</div>
+
+<!-- about section start -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.</p>
+      <p>Praesent at sapien a neque mollis commodo ac ac magna. Aliquam erat volutpat. Aenean at justo sit amet tellus euismod blandit gravida vel augue. Duis ut dictum mi. In eros arcu, ultrices nec lorem ut, pretium ullamcorper dolor. Praesent pretium sagittis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>  
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>  
+   </div>
+
+</section>
+
+<!-- about section end -->
+
+<!-- reviews section start -->
+
+<section class="reviews">
+
+<h1 class="heading-title"> clients reviews</h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-1.png" alt="">         
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-2.png" alt="">         
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-3.png" alt="">         
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-4.png" alt="">         
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula. Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula.Praesent at sapien a neque mollis commodo ac ac magna. Aliquam erat volutpat. Aenean at justo sit amet tellus euismod blandit gravida vel augue. Duis ut dictum mi. In eros arcu, ultrices nec lorem ut, pretium ullamcorper dolor. Praesent pretium sagittis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-5.png" alt="">         
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Donec at est vitae purus porta mollis. Praesent non elementum nulla. Curabitur egestas felis et sem consectetur consectetur.consectetur adipiscing elit. Sed at dui vel est dignissim tristique vitae sed ligula.Praesent at sapien a neque mollis commodo ac ac magna. Aliquam erat volutpat. Aenean at justo sit amet tellus euismod blandit gravida vel augue. Duis ut dictum mi. In eros arcu, ultrices nec lorem ut, pretium ullamcorper dolor. Praesent pretium sagittis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-6.png" alt="">         
+         </div>
+  
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+   </div>
+
+</section>
+
+
+
+
+<!-- reviews section end -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <!-- footer section starts -->
+
+ <section class="footer">
+   
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333</a>
+         <a href="#"> <i class="fas fa-envelope"></i> radovanovic@gmail.com</a>
+         <a href="#"> <i class="fas fa-map"></i> novi sad, serbia - 21000</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Srdjan Radovanovic</span> | all rights reserved! </div>
+
+ </section>
+
+ <!-- footer section ends -->
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ book_db.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 05, 2022 at 02:44 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `book_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_form`
+--
+
+CREATE TABLE `book_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `guests` int(255) NOT NULL,
+  `arrivals` date NOT NULL,
+  `leaving` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_form`
+--
+
+INSERT INTO `book_form` (`id`, `name`, `email`, `phone`, `address`, `location`, `guests`, `arrivals`, `leaving`) VALUES
+(1, 'srdjan', 'srdjan@gmail.com', '90259235', 'aksfkasmf', 'hawaii', 11, '2022-07-08', '2022-07-30'),
+(2, 'nemanja', 'nemanja@gmail.com', '0623423492', 'askjfajsk 20', 'bosna', 2, '2022-07-13', '2022-07-27');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_form`
+--
+ALTER TABLE `book_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_form`
+--
+ALTER TABLE `book_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
